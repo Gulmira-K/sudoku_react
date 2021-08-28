e44a53683172238e67b9a708f87457c0ead3dd74
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-08-28 11:32:00 +0200

    created the project

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import Table from "./Table/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sudoku</h1>
+      </header>
+      <main>
+        <Table />
+      </main>
+      <footer>
+        Coded by Gulmira Kojenova
+      </footer>
+    </div>
+  );
+}
+
+export default App;
