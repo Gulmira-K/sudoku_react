107b677820281fdd0a11cd181e914ba018909a37
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-09-02 09:03:57 +0200

    added functions to fill up the cells

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,27 +1,43 @@
+import { useEffect } from 'react';
 import './Block.scss';
 
-const Block = () => {
-  const length = 3,
-        grid = Array(length).fill([]).map(() => {
-          return Array(length).fill('');
-        });
+type BlockProps = {
+  rowColumnLength: number,
+  grid: any,
+  randomPlace: any,
+  randomNumber: any
+}
 
+const Block = ({rowColumnLength, grid, randomPlace, randomNumber}: BlockProps) => {
   let matrix = '', i, j;
 
-  for (i = 0; i < length; i += 1) {
+  for (i = 0; i < rowColumnLength; i += 1) {
     matrix += '<div class="row">';
 
-    for (j = 0; j < length; j += 1) {
+    for (j = 0; j < rowColumnLength; j += 1) {
       matrix += `<div class="cell">${grid[i][j]}</div>`
+
     }
 
     matrix += '</div>'
   }
-console.log(matrix)
 
-  
+  const initGame = () => {
+    grid[randomPlace()][randomPlace()] = randomNumber();
+    grid[randomPlace()][randomPlace()] = randomNumber();
+    grid[randomPlace()][randomPlace()] = randomNumber();
+    grid[randomPlace()][randomPlace()] = randomNumber();
+    grid[randomPlace()][randomPlace()] = randomNumber();
+    grid[randomPlace()][randomPlace()] = randomNumber();
+
+  }
+
+  useEffect(() => {
+    initGame();
+  }, [])
+
   return (
-    <div className='BlockContainer' dangerouslySetInnerHTML={{__html: matrix}}>
+    <div className='BlockContainer' dangerouslySetInnerHTML={{ __html: matrix }}>
     </div>
   )
 }
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,18 +1,45 @@
+import { useEffect } from 'react';
 import Block from '../Block/Block'
 import './Grid.scss';
 
 const Grid = () => {
+  const rowColumnLength = 3,
+      grid = Array(rowColumnLength).fill([]).map(() => {
+        return Array(rowColumnLength).fill('');
+      });
+
+  const randomPlace =()=> {
+    return Math.floor(Math.random() * (rowColumnLength - 0) + 0);
+  }
+
+  const randomNumber = () => {
+    return Math.floor(Math.random() * (9 - 1) + 1);
+  }
+
+  // const initGame = () => {
+  //   grid[randomPlace()][randomPlace()] = randomNumber();
+  //   grid[randomPlace()][randomPlace()] = randomNumber();
+  //   grid[randomPlace()][randomPlace()] = randomNumber();
+  //   grid[randomPlace()][randomPlace()] = randomNumber();
+  //   grid[randomPlace()][randomPlace()] = randomNumber();
+  //   grid[randomPlace()][randomPlace()] = randomNumber();
+  // }
+
+  // useEffect(() => {
+  //   initGame();
+  // }, [])
+
   return (
     <div className='GridContainer'>
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
+      <Block rowColumnLength={rowColumnLength} grid={grid} randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
+      <Block rowColumnLength={rowColumnLength} grid={grid}  randomPlace={randomPlace} randomNumber={randomNumber}/>
     </div>
   )
 }
